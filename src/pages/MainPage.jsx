b96aecb15bc9460464ec7ee44169aba225b4f9e0
samuelys <samuelys@naver.com>
2022-09-24 10:56:00 +0900

    feat: 메인 페이지 UI 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+import styled from '@emotion/styled';
+
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+
+import Slider from '../components/Slider/Slider';
+import MainTopSection from "../section/MainSection/MainTopSection";
+
+
+
+const Main = styled.main`
+    max-width: 1200px;
+    margin: 0 auto;
+`;
+
+const Container = styled.div`
+    margin: 62px;
+    padding: 8px 12px;
+    height: auto;
+    // box-shadow: 3px 3px 5px 5px grey;
+`;
+
 
 const MainPage = () => {
     return (
-        <h1>Main Page</h1>
+        <>
+            <Header />
+            <Main>
+                <Container>
+                    <MainTopSection />
+                    <Slider />
+                </Container>
+            </Main>
+            <Footer />
+        </>
     )
 };
 
