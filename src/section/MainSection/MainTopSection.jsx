3230115ddac1a4663e65c38f49e8362c4d278ed7
samuelys <samuelys@naver.com>
2022-09-24 11:02:15 +0900

    feat: 메인 페이지 Top Section 구성

---
+++ src/section/MainSection/MainTopSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Buttons from "../../components/Button/Buttons";
+import SliderBar from "../../components/SliderBar/SliderBar";
+
+const MainTopSection = () => {
+    return(
+        <>
+            <Buttons />
+            <SliderBar />
+        </>
+    );
+};
+
+export default MainTopSection;
